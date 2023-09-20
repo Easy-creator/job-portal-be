3fa4462215d9248283fa2bc8bc3d7bde8197b874
Ezekiel--dev <81721664+Easy-creator@users.noreply.github.com>
2023-09-20 13:57:01 +0100

    Auth in progress

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from .views import RegisterApiView, LoginApiView, AuthUserApiView
+from django.urls import path
+
+app_name = 'users'
+
+urlpatterns = [
+    path('register/', RegisterApiView.as_view(), name="register"), #To register user
+    path('login/', LoginApiView.as_view(), name="login"), #To login user
+    path('user/', AuthUserApiView.as_view(), name="user"), # testing authentication
+
+]
