3ab07ffdb73dd093ffad50d9fe4040d1b48c1ffb
Ezekiel--dev <ezekielizuchi2018@gmail.com>
2023-09-20 16:34:05 +0100

    update

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls', namespace="users")),
+    path('users/', include('users.urls', namespace="users")),
     # path('account/', include('account.urls', namespace="account")),
 ]
 
